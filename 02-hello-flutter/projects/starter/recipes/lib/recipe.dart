b21cdab32190d0bf7a9640ef11d0505ac633042e
bimri <bimri@outlook.com>
2021-11-16 01:38:13 +0300

    Making it look nice

--- 02-hello-flutter/projects/starter/recipes/lib/recipe.dart
+++ 02-hello-flutter/projects/starter/recipes/lib/recipe.dart
@@ -9,14 +9,14 @@ class Recipe {
   );
 
   static List<Recipe> samples = [
-    Recipe(
-      'Spaghetti and Meatballs',
-      'assets/2126711929_ef763de2b3_w.jpg',
-    ),
-    Recipe(
-      'Tomato Soup',
-      'assets/27729023535_a57606c1be.jpg',
-    ),
+    // Recipe(
+    //   'Spaghetti and Meatballs',
+    //   'assets/2126711929_ef763de2b3_w.jpg',
+    // ),
+    // Recipe(
+    //   'Tomato Soup',
+    //   'assets/27729023535_a57606c1be.jpg',
+    // ),
     Recipe(
       'Grilled Cheese',
       'assets/3187380632_5056654a19_b.jpg',
