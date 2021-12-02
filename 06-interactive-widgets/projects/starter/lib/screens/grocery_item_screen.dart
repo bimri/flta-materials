ff4f508cee0d04f2e0890253cba9902a4a887f74
bimri <bimri@outlook.com>
2021-12-02 18:57:01 +0300

    Adding GroceryItemScreen’s state properties

---
+++ 06-interactive-widgets/projects/starter/lib/screens/grocery_item_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:intl/intl.dart';
+import 'package:uuid/uuid.dart';
+import '../models/models.dart';
+
+class GroceryItemScreen extends StatefulWidget {
+  // 1
+  final Function(GroceryItem) onCreate;
+  // 2
+  final Function(GroceryItem) onUpdate;
+  // 3
+  final GroceryItem? originalItem;
+  // 4
+  final bool isUpdating;
+
+  const GroceryItemScreen({
+    Key? key,
+    required this.onCreate,
+    required this.onUpdate,
+    this.originalItem,
+  })  : isUpdating = (originalItem != null),
+        super(key: key);
+
+  @override
+  _GroceryItemScreenState createState() => _GroceryItemScreenState();
+}
+
+class _GroceryItemScreenState extends State<GroceryItemScreen> {
+  final _nameController = TextEditingController();
+  String _name = '';
+  Importance _importance = Importance.low;
+  DateTime _dueDate = DateTime.now();
+  TimeOfDay _timeOfDay = TimeOfDay.now();
+  Color _currentColor = Colors.green;
+  int _currentSliderValue = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    // 1
+    final originalItem = widget.originalItem;
+    if (originalItem != null) {
+      _nameController.text = originalItem.name;
+      _name = originalItem.name;
+      _currentSliderValue = originalItem.quantity;
+      _importance = originalItem.importance;
+      _currentColor = originalItem.color;
+      final date = originalItem.date;
+      _timeOfDay = TimeOfDay(hour: date.hour, minute: date.minute);
+      _dueDate = date;
+    }
+
+    // 2
+    _nameController.addListener(() {
+      setState(() {
+        _name = _nameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO 12: Add GroceryItemScreen Scaffold
+    return Container(color: Colors.orange);
+  }
+
+  // TODO: Add buildNameField()
+
+  // TODO: Add buildImportanceField()
+
+  // TODO: ADD buildDateField()
+
+  // TODO: Add buildTimeField()
+
+  // TODO: Add buildColorPicker()
+
+  // TODO: Add buildQuantityField()
+
+}
