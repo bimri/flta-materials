6e5cd305103bd660b3d69287dc16809878f76111
bimri <bimri@outlook.com>
2021-12-06 22:19:27 +0300

    Creating a new grocery item

--- 07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
+++ 07-routes-and-navigation/projects/starter/lib/navigation/app_router.dart
@@ -57,8 +57,33 @@ class AppRouter extends RouterDelegate
         if (appStateManager.isOnboardingComplete)
           Home.page(appStateManager.getSelectedTab),
 
-        // TODO: Create new item
-        // TODO: Select GroceryItemScreen
+        // 1
+        if (groceryManager.isCreatingNewItem)
+// 2
+          GroceryItemScreen.page(
+            onCreate: (item) {
+              // 3
+              groceryManager.addItem(item);
+            },
+            onUpdate: (item, index) {
+              // 4 No update
+            },
+          ),
+
+        // 1
+        if (groceryManager.selectedIndex != -1)
+          // 2
+          GroceryItemScreen.page(
+              item: groceryManager.selectedGroceryItem,
+              index: groceryManager.selectedIndex,
+              onUpdate: (item, index) {
+                // 3
+                groceryManager.updateItem(item, index);
+              },
+              onCreate: (_) {
+                // 4 No create
+              }),
+
         // TODO: Add Profile Screen
         // TODO: Add WebView Screen
       ],
@@ -78,7 +103,10 @@ class AppRouter extends RouterDelegate
 
     // 5
     // TODO: Handle Onboarding and splash
-    // TODO: Handle state when user closes grocery item screen
+    if (route.settings.name == FooderlichPages.groceryItemDetails) {
+      groceryManager.groceryItemTapped(-1);
+    }
+
     // TODO: Handle state when user closes profile screen
     // TODO: Handle state when user closes WebView screen
     // 6
