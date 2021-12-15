0f310131d1f40479d1dbb989b42e3728c4cee7dc
bimri <bimri@outlook.com>
2021-12-15 23:34:30 +0300

    Converting a URL string to an AppLink

--- 08-deep-links-and-web-URLs/projects/starter/lib/navigation/app_link.dart
+++ 08-deep-links-and-web-URLs/projects/starter/lib/navigation/app_link.dart
@@ -21,7 +21,26 @@ class AppLink {
     this.itemId,
   });
 
-// TODO: Add fromLocation
+  static AppLink fromLocation(String? location) {
+    // 1
+    location = Uri.decodeFull(location ?? '');
+    // 2
+    final uri = Uri.parse(location);
+    final params = uri.queryParameters;
+
+    // 3
+    final currentTab = int.tryParse(params[AppLink.tabParam] ?? '');
+    // 4
+    final itemId = params[AppLink.idParam];
+    // 5
+    final link = AppLink(
+      location: uri.path,
+      currentTab: currentTab,
+      itemId: itemId,
+    );
+    // 6
+    return link;
+  }
 
 // TODO: Add toLocation
 
